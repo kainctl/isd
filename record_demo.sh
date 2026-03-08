d6292acb64b2ecba24a076704e55f4e02b870b68
kainctl <safe.boat9582@fastmail.com>
2026-03-08 19:42:32 +0100

    docs: fix recording

--- record_demo.sh
+++ record_demo.sh
@@ -1,6 +1,6 @@
 set -exu
 
-mv ./docs/assets/images/isd.cast ./docs/assets/images/isd.bak.cast
+mv ./docs/assets/images/isd.cast ./docs/assets/images/isd.bak.cast || true
 
 systemctl --user cat 0-isd-example-unit-01.service 2&> /dev/null || \
   nix run ./nix/systemd-units#generate-doc-test-data
